a4149ff48ab711d25890626fa83c90f22366eacb
1619kHz <37778292+everknwon@users.noreply.github.com>
2020-10-29 17:47:29 +0800

    test: 更新测试用例

--- src/test/java/example/service/UserServiceImpl.java
+++ src/test/java/example/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package example.service;
 
+import org.apex.annotation.Singleton;
+
 /**
  * @author WangYi
  * @since 2020/10/29
  */
+@Singleton
 public class UserServiceImpl implements UserService {
 
   @Override
